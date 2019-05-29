fea953276591a51366ecd2704f69d2a843b28870
pinkig15 <pinkigautam15@gmail.com>
2019-05-29 10:45:59 +0530

    show countdown using simple react

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './App.css';
+import Clock from './Clock';
+import {Form, FormControl, Button} from 'react-bootstrap';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            deadline: 'December 25, 2018',
+            newDeadline: ''
+        }
+    }
+
+    changeDealine() {
+        this.setState({deadline: this.state.newDeadline});
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-title">
+                    Countdown to {this.state.deadline}
+                </div>
+                <Clock 
+                    deadline={this.state.deadline}
+                />
+                <Form inline>
+                    <FormControl
+                        className="Deadline-input"
+                    placeholder="new date"
+                        onChange={event => this.setState({newDeadline: event.target.value})}
+                    />
+                    <Button onClick={() => this.changeDealine()}>
+                        Submit
+                    </Button>
+                </Form>
+            </div>
+        )
+    }
+}
+
+export default App;
